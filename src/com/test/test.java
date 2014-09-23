1de25ad27614e23e22878d7f29ecc4e8842d9c1b
Aditya Gupta <aditya@bsb.in>
2014-09-23 13:37:00 +0530

    logging change

--- src/com/test/test.java
+++ src/com/test/test.java
@@ -3,7 +3,7 @@ package com.test;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("this is test class");
+		System.out.println("this is testing class");
 
 	}
 
